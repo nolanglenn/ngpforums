3d3b1ff0b0d25306c6c5a5d85ab9f936457d89b6
unknown <nolangglenn@gmail.com>
2021-01-08 10:02:37 -0600

    Added Redis and cookie manager

--- server/dist/resolvers/post.js
+++ server/dist/resolvers/post.js
@@ -51,7 +51,7 @@ let PostResolver = class PostResolver {
             return post;
         });
     }
-    deletePost(id, title, { em }) {
+    deletePost(id, { em }) {
         return __awaiter(this, void 0, void 0, function* () {
             yield em.nativeDelete(Post_1.Post, { id });
             return true;
@@ -93,10 +93,9 @@ __decorate([
 __decorate([
     type_graphql_1.Mutation(() => Boolean),
     __param(0, type_graphql_1.Arg('id')),
-    __param(1, type_graphql_1.Arg('title', () => String, { nullable: true })),
-    __param(2, type_graphql_1.Ctx()),
+    __param(1, type_graphql_1.Ctx()),
     __metadata("design:type", Function),
-    __metadata("design:paramtypes", [Number, String, Object]),
+    __metadata("design:paramtypes", [Number, Object]),
     __metadata("design:returntype", Promise)
 ], PostResolver.prototype, "deletePost", null);
 PostResolver = __decorate([
